75403be4ba1d0e9c977e329059bd234390113a05
Connor Sullivan <cksullvn@gmail.com>
2019-10-04 19:23:40 -0500

    updated styling on eat.js and eat.scss to meet moqup standards

--- src/javascripts/components/eat.js
+++ src/javascripts/components/eat.js
@@ -2,39 +2,40 @@ import '../../styles/eat.scss';
 import utilities from '../helpers/utilities';
 
 const increaseFullLength = () => {
-  let currentLength = document.getElementById('myProgress').value;
+  let currentLength = document.getElementById('full').value;
   if (currentLength < 90) {
     currentLength += 10;
-    document.getElementById('myProgress').value = currentLength;
+    document.getElementById('full').value = currentLength;
   } else if ((currentLength > 90 && currentLength < 100)) {
     currentLength = 100;
-    document.getElementById('myProgress').value = 100;
+    document.getElementById('full').value = 100;
   }
 };
 
 const decreaseFullLength = () => {
-  let currentLength = document.getElementById('myProgress').value;
+  let currentLength = document.getElementById('full').value;
   if (currentLength > 1) {
     currentLength -= 3;
-    document.getElementById('myProgress').value = currentLength;
+    document.getElementById('full').value = currentLength;
   } else if (currentLength > 0 && currentLength < 3) {
     currentLength = 0;
-    document.getElementById('myProgress').value = currentLength;
+    document.getElementById('full').value = currentLength;
   }
 };
 
 const eatPrinter = () => {
   const domString = `
-<h2>Eat</h2>
-<progress id="myProgress" value="100" max="100"></progress>
+<progress id="full" value="100" max="100"></progress>
 <br>
-<button id="healthy">Healthy Food</button>
-<button id="unhealthy">Unhealthy Food</button>
+<div id="buttonwrapper">
+<h2>Eat</h2>
+<button id="healthy">Salad</button>
+<button id="unhealthy">Pie</button>
+</div>
  `;
   utilities.printToDom('eat', domString);
   document.getElementById('healthy').addEventListener('click', increaseFullLength);
   document.getElementById('unhealthy').addEventListener('click', decreaseFullLength);
 };
-
 
 export default { eatPrinter };
